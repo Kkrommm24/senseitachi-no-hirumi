2db8b29d7f6a7d31a26de248095ba2dce4092864
HoangNH2403 <nguyenhuyhoang242003>
2024-12-07 16:41:15 +0700

    Fix: add username comment, isFavourite

--- be/src/routes/getfooddetailsRoutes.js
+++ be/src/routes/getfooddetailsRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getFoodDetails } from '../controllers/getfooddetailsController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // Route tìm kiếm món ăn
-router.get('/get-food-detail', getFoodDetails);
+router.get('/get-food-detail',authMiddleware, getFoodDetails);
 
 export default router;
